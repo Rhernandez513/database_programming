0b3a07f252445fbee1464babd29f97ca888e933d
cprintzis <cprintzis@luc.edu>
2018-04-19 18:03:04 -0500

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,41 @@
+
+/*Employee Table*/
+INSERT INTO `Employee`(`Staff ID`, `Date Hired`, `Job Title`)
+VALUES ('60','6/11/2011','Groomer');
+
+INSERT INTO `Employee`(`Staff ID`, `Date Hired`, `Job Title`)
+VALUES ('61','4/20/2010','Reception');
+
+INSERT INTO `Employee`(`Staff ID`, `Date Hired`, `Job Title`)
+VALUES ('62','8/14/2008','Supervisor');
+
+INSERT INTO `Employee`(`Staff ID`, `Date Hired`, `Job Title`)
+VALUES ('63','5/03/2008','Maintenance');
+
+/*Dog Table*/
+INSERT INTO `Dog Table`(`Dog ID`, `Dog Name`, `Year of Birth`, `Photo`, `Sex`, `Cage Number`, `Breed`, `Adoption Status`, `Weight`)
+VALUES ('111','Violet','2013','	https://imgur.com/a/INSQ7MJ','F','24','Shiba Inu','Yes','20');
+
+INSERT INTO `Dog Table`(`Dog ID`, `Dog Name`, `Year of Birth`, `Photo`, `Sex`, `Cage Number`, `Breed`, `Adoption Status`, `Weight`)
+VALUES ('112','Lily','2008','https://imgur.com/a/9cVgBqj','F','13','Pitbull Mix','Yes','30');
+
+INSERT INTO `Dog Table`(`Dog ID`, `Dog Name`, `Year of Birth`, `Photo`, `Sex`, `Cage Number`, `Breed`, `Adoption Status`, `Weight`)
+VALUES ('113','Mya','2009','https://imgur.com/a/wCiDPv6','F','3','Pug','No','12');
+
+INSERT INTO `Dog Table`(`Dog ID`, `Dog Name`, `Year of Birth`, `Photo`, `Sex`, `Cage Number`, `Breed`, `Adoption Status`, `Weight`)
+VALUES ('114','Honey','2016','https://imgur.com/a/wCiDPv6','F','5','Siberian Husky','No','65');
+
+INSERT INTO `Dog Table`(`Dog ID`, `Dog Name`, `Year of Birth`, `Photo`, `Sex`, `Cage Number`, `Breed`, `Adoption Status`, `Weight`)
+VALUES ('115','Peter','2017','https://imgur.com/a/i2HdAKQ','M','10','Cavachon','No','10');
+
+INSERT INTO `Dog Table`(`Dog ID`, `Dog Name`, `Year of Birth`, `Photo`, `Sex`, `Cage Number`, `Breed`, `Adoption Status`, `Weight`)
+VALUES ('116','Edie','2018','https://imgur.com/a/ZQuDhtW','F','20','Pitbull Mix','No','25');
+
+INSERT INTO `Dog Table`(`Dog ID`, `Dog Name`, `Year of Birth`, `Photo`, `Sex`, `Cage Number`, `Breed`, `Adoption Status`, `Weight`)
+VALUES ('117','Tank','2018','https://imgur.com/a/QECk0VK','M','14','Rotweiler','No','13');
+
+INSERT INTO `Dog Table`(`Dog ID`, `Dog Name`, `Year of Birth`, `Photo`, `Sex`, `Cage Number`, `Breed`, `Adoption Status`, `Weight`)
+VALUES ('118','Bubba','2016','https://imgur.com/a/4GO4d5A','M','18','French Bulldog','No','25');
+
+INSERT INTO `Dog Table`(`Dog ID`, `Dog Name`, `Year of Birth`, `Photo`, `Sex`, `Cage Number`, `Breed`, `Adoption Status`, `Weight`)
+VALUES ('119','Max','2018','https://imgur.com/Bcs2AYD','M','11','Labrador','No','10');
