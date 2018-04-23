6794b540c1dfda59e0524cc8493a18e58265dcc0
Robert Hernandez <rhernandez513@gmail.com>
2018-04-23 17:45:54 -0500

    tried a different method to do sql query, same negative result

--- function_new_activity.html.php
+++ function_new_activity.html.php
@@ -24,11 +24,19 @@ echo $result;
 
 function New_Activity($dog_id, $staff_id, $activity, $date) {
     global $pdo;
-    $query = "INSERT INTO `ActivityRecord` (`Dog ID`, `Staff ID`, `Activity`, `Date`) VALUES ($dog_id, $staff_id, $activity, '$date');";
-    $result = $pdo->exec($query);
-    $row = $result->fetchAll();
+//    $query = "INSERT INTO `ActivityRecord` (`Dog ID`, `Staff ID`, `Activity`, `Date`) VALUES ($dog_id, $staff_id, $activity, '$date');";
 
-    return row;
+    $sql = 'INSERT INTO `ActivityRecord` SET 
+            dog id = :dog_id,
+            staff id = :staff_id,
+            activity = :activity
+            date = :date';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':dog_id', $_POST['dog_id']);
+    $s->bindValue(':staff_id', $_POST['staff_id']);
+    $s->bindValue(':activity', $_POST['activity']);
+    $s->bindValue(':date', $_POST['date']);
+    $s->execute();
 }
 
 ?>
