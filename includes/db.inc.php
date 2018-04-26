c60dad17b8a34373dbc9fe44b15faa901e219c62
Robert David Hernandez <Rhernandez513@users.noreply.github.com>
2018-04-26 16:57:25 -0500

    Update db.inc.php

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,9 +1,9 @@
 <?php
 try
 {
-  // $pdo = new PDO('mysql:host=localhost;dbname=puppy_management_system', 'root', '');
+  $pdo = new PDO('mysql:host=localhost;dbname=puppy_management_system', 'root', '');
   // PORT 3306
-  $pdo = new PDO('mysql://m1kobxzpnsszko0l:zwtopo526l8hst9a@h7xe2knj2qb6kxal.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/opj7d9c1hlsmn8to;dbname=opj7d9c1hlsmn8to', 'm1kobxzpnsszko0l', ' zwtopo526l8hst9a ');
+  // $pdo = new PDO('mysql://m1kobxzpnsszko0l:zwtopo526l8hst9a@h7xe2knj2qb6kxal.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/opj7d9c1hlsmn8to;dbname=opj7d9c1hlsmn8to', 'm1kobxzpnsszko0l', ' zwtopo526l8hst9a ');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->exec('SET NAMES "utf8"');
 }
