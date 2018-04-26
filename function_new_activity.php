9a8fc316a1c93106c8ceb5591a97e387a31cb880
Robert David Hernandez <Rhernandez513@users.noreply.github.com>
2018-04-26 16:54:43 -0500

    Update function_new_activity.php

--- function_new_activity.php
+++ function_new_activity.php
@@ -80,27 +80,45 @@
 
 <?php
 
+include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+
+$result = "No Result from DB";
+
+if(isset($_POST['dog_id']) && !empty($_POST['dog_id'])) {
+    $dog_id = $_POST['dog_id'];
+}
+if(isset($_POST['staff_id']) && !empty($_POST['staff_id'])) {
+    $staff_id = $_POST['staff_id'];
+}
+if(isset($_POST['activity']) && !empty($_POST['activity'])) {
+    $activity = $_POST['activity'];
+}
+if(isset($_POST['date']) && !empty($_POST['date'])) {
+    $date = $_POST['date'];
+}
+
+$result = New_Activity($dog_id, $staff_id, $activity, $date);
+
+echo $result;
+
+function New_Activity($dog_id, $staff_id, $activity, $date) {
+    global $pdo;
 try{
-$pdo = new PDO('mysql:host=localhost;dbname=ShelterDB', 'root', 'root');
-
-    $sql = "INSERT INTO ActivityRecord SET
-            dog_id = :dog_id,
-            staff_id = :staff_id,
-            activity = :activity,
-            date = :date;";
-    $s = $pdo->prepare($sql);
-    $s->bindValue(':dog_id', $_POST['dog_id']);
-    $s->bindValue(':staff_id', $_POST['staffid']);
-    $s->bindValue(':activity', $_POST['activity']);
-    $s->bindValue(':date', $_POST['date']);
-    $s->execute();
-
-    echo 'The activity has been inserted.';
-  }
-  catch(PDOException $e)
-  {
+    $stmt = $pdo->prepare("INSERT INTO ActivityRecord (`Dog ID`, `Staff ID`, `Activity`, `Date`)
+    VALUES (:dogid, :staffid, :activity, :date)");
+    $stmt->bindParam(':dogid', $dog_id);
+    $stmt->bindParam(':staffid', $staff_id);
+    $stmt->bindParam(':activity', $activity);
+    $stmt->bindParam(':date', $date);
+    $stmt->execute();
+	echo 'The activity has been inserted.';
+    return $stmt->fetchAll();
+	}
+catch(PDOException $e)
+{
     echo 'The activity has not been entered.';
-  }
+}
+}
 include 'entered_records.php';
 ?>
 </head>
