ab84123c3d8ea6eac6ec430359fe09866ad21185
cprintzis <cprintzis@luc.edu>
2018-05-02 19:22:39 -0500

    doesn't work if you want to add multiple dog ids

--- function_new_activity.html.php
+++ function_new_activity.html.php
@@ -99,11 +99,36 @@ try{
     $stmt->bindParam(':activity', $activity);
     $stmt->bindParam(':date', $date);
     $stmt->execute();
+
 	echo 'The activity has been inserted.';
+
+  $query = "SELECT * FROM ActivityRecord";
+  $result = $pdo->query($query);
+  $row = $result->fetchAll();  ?>
+
+  <title>Updated Actitivites</title>
+<head>
+<body>
+    <p> Here are all the activities: </p>
+    <table>
+      <th>Dog ID</th>
+      <th>Staff ID</th>
+      <th>Activity</th>
+      <th>Date</th>
+      <?php foreach($row as $value): ?>
+              <tr>
+                <td> <?php echo $value['Dog ID']; ?> </td>
+                    <td> <?php echo $value['Staff ID']; ?> </td>
+                        <td> <?php echo $value['Activity']; ?> </td>
+                            <td> <?php echo $value['Date']; ?> </td>
+              </tr>
+      <?php endforeach ?>
+
+   </table> <?php
 	}
 catch(PDOException $e)
 {
-    echo 'The activity has not been entered.';
+    echo 'The activity has not been entered. Please enter valid dog and staff ids';
 }
 }
 include 'entered_records.html.php';
