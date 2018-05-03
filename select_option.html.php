6be0bbd85a51dbcfcccd1f97b6507c313aab3fa5
GraceBiasco <gbiasco41@gmail.com>
2018-05-03 00:21:25 -0500

    Update select_option.html.php

--- select_option.html.php
+++ select_option.html.php
@@ -18,7 +18,7 @@ $row = $result->fetchAll();
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Round About - Start Bootstrap Template</title>
+    <title>Employee Dashboard</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -29,34 +29,26 @@ $row = $result->fetchAll();
   </head>
 
   <body>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
+
+    <header>
+      <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <ul class="list-unstyled">
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </nav>
-
+      <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <h4 class="text-white">Shelter Database &#x2764;</h4>
+        </div>
+      </div>
+    </header>
     <!-- Page Content -->
     <div class="container">
         <br>
@@ -66,8 +58,7 @@ $row = $result->fetchAll();
           <h2>You have logged in<h2>
 
       </h1>
-          <p>Dog ID:</p>
-	      <br>
+<br>
         <select id="dog-id-select">
           <option disabled selected>Please choose a dog...</option>
           <?php
@@ -86,63 +77,21 @@ $row = $result->fetchAll();
         </div>
         <div class="col-lg-4 col-sm-6 text-center mb-4">
             <a href="records.html.php"><img src="/img/syringe.jpg"></a>
-          <h3>Records
-            <br>
-            <small>View Medical Records</small>
-          </h3>
-          <p>Description here</p>
+          <h3>Medical Records</h3>
         </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img src="/img/walk_dog.jpg">
-          <h3>Walk Dog
-		  <br>
-            <small>Click Here to enter dog walking Activity</small>
-          </h3>
-          <p> Description here </p>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-            <a href="population.html.php"><img src="/img/groom_dog.jpg"></a>
-            <h3><a href="population.html.php">Population </a>
-		  <br>
-            <small>Check on Dog population</small>
-          </h3>
-          <p>Description here</p>
-        </div>
+
         <div class="col-lg-4 col-sm-6 text-center mb-4">
           <a href="/volunteerView.html.php"><img src="/img/volunteers.png"></a>
-          <h3>Volunteers
-		  <br>
-            <small>Click here to view Volunteers</small>
-          </h3>
-          <p>Description</p>
+          <h3>Activity Records</h3>
         </div>
+
         <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img src="/img/food.jpg">
-          <h3>Order Supplies
-            <br>
-            <small>Click here to order supplies</small>
-          </h3>
-          <p>Description</p>
+            <a href="population.html.php"><img src="/img/groom_dog.jpg"></a>
+            <h3>Total Dogs We Have Saved</h3>
         </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img src="/img/certificate.jpg">
-          <h3>View Adoption Record
-            <small>Click here to view adoption records</small>
-          </h3>
-          <p>Description</p>
-        </div>
-      </div>
+
 
     </div>
-    <!-- /.container -->
-
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
-      </div>
-      <!-- /.container -->
-    </footer>
 
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
